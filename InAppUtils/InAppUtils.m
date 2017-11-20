dc188ad0466686a4df81e9aca17d35d50926027a
Danny Turcotte <danny.turcotte@exposiohdr.com>
2017-11-20 11:47:17 -0500

    Add quantity

--- InAppUtils/InAppUtils.m
+++ InAppUtils/InAppUtils.m
@@ -90,6 +90,7 @@ RCT_EXPORT_MODULE()
 }
 
 RCT_EXPORT_METHOD(purchaseProductForUser:(NSString *)productIdentifier
+                  quantity:(NSNumber *)quantity
                   username:(NSString *)username
                   callback:(RCTResponseSenderBlock)callback)
 {
@@ -97,12 +98,14 @@ RCT_EXPORT_METHOD(purchaseProductForUser:(NSString *)productIdentifier
 }
 
 RCT_EXPORT_METHOD(purchaseProduct:(NSString *)productIdentifier
+                  quantity:(NSNumber *)quantity
                   callback:(RCTResponseSenderBlock)callback)
 {
     [self doPurchaseProduct:productIdentifier username:nil callback:callback];
 }
 
 - (void) doPurchaseProduct:(NSString *)productIdentifier
+                  quantity:(NSNumber *)quantity
                   username:(NSString *)username
                   callback:(RCTResponseSenderBlock)callback
 {
@@ -117,6 +120,9 @@ RCT_EXPORT_METHOD(purchaseProduct:(NSString *)productIdentifier
 
     if(product) {
         SKMutablePayment *payment = [SKMutablePayment paymentWithProduct:product];
+        if (quantity) {
+            payment.quantity = quantity;
+        }
         if(username) {
             payment.applicationUsername = username;
         }
@@ -134,7 +140,7 @@ restoreCompletedTransactionsFailedWithError:(NSError *)error
     NSString *key = RCTKeyForInstance(@"restoreRequest");
     RCTResponseSenderBlock callback = _callbacks[key];
     if (callback) {
-        switch (error.code)     
+        switch (error.code)
         {
             case SKErrorPaymentCancelled:
                 callback(@[@"user_cancelled"]);
@@ -143,7 +149,7 @@ restoreCompletedTransactionsFailedWithError:(NSError *)error
                 callback(@[@"restore_failed"]);
                 break;
         }
-        
+
         [_callbacks removeObjectForKey:key];
     } else {
         RCTLogWarn(@"No callback registered for restore product request.");
